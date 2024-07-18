f221a2456c91b297fdc7a994439bfe8be7ff4f42
yakhya <abdouyakhya1@gmail.com>
2024-07-18 11:43:47 +0000

    hidding .env

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-//--------------------------connection à la base de données---------------------------------
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() =>console.log("successful connection"))
-.catch((error) => console.log(error)) 
